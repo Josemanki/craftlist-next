68533957098a24fd0f929b8186c2d1de03f8fbe0
Jose Hernandez <josemanki@gmail.com>
2023-03-30 23:13:09 +0100

    Sets up favicon and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,11 +13,37 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Dofus Craftlist</title>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta content="Dofus Craftlist" property="og:title" />
+        <meta
+          content="Make your own shopping list when you are feeling like crafting!"
+          property="og:description"
+        />
+        <meta
+          content="https://dofuscraftlist.vercel.app/app.jpeg"
+          property="og:image"
+        />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <QueryClientProvider client={queryClient}>
